bfba449c8db3a611d87d51107a4bd62b2cff160b
Alvaro Romero <alromero@redhat.com>
2024-05-14 20:57:40 +0200

    Implement retry mechanism in vmexport download
    This commit implements a retry mechanism in the vmexport download command so, when the export server returns an unexpected status, we retry the download again.
    
    Signed-off-by: Alvaro Romero <alromero@redhat.com>

--- pkg/storage/export/virt-exportserver/exportserver.go
+++ pkg/storage/export/virt-exportserver/exportserver.go
@@ -472,7 +472,7 @@ func archiveHandler(mountPoint string) http.Handler {
 			return
 		}
 		if hasPermissions := checkDirectoryPermissions(mountPoint); !hasPermissions {
-			w.WriteHeader(http.StatusForbidden)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
@@ -530,7 +530,7 @@ func gzipHandler(filePath string) http.Handler {
 		f, err := os.Open(filePath)
 		if err != nil {
 			log.Log.Reason(err).Errorf("error opening %s", filePath)
-			w.WriteHeader(http.StatusForbidden)
+			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 		defer f.Close()
